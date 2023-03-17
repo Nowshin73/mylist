5d5a0d1f25d63e7ce1de443cfd210faac6f3eba4
NowshinNawar <68994761+Nowshin73@users.noreply.github.com>
2023-03-17 23:27:01 +0600

    todo list

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('createbtn').addEventListener('click',function(){
+    //window.location.href="list.html"; 
+    const createlist = document.getElementById("createList");
+    createlist.classList.add = 'd-none';
+});
