c63be599addd9c524fa07e8fb599e9d1eb788b34
NowshinNawar <68994761+Nowshin73@users.noreply.github.com>
2023-03-18 23:20:03 +0600

    create list fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+
 document.getElementById('createbtn').addEventListener('click',function(){
-    //window.location.href="list.html"; 
-    const createlist = document.getElementById("createList");
-    createlist.classList.add = 'd-none';
+  const  inputTitle = document.getElementById("input-title").value;
+  const createList = document.getElementById('createList');
+  const modalcon = document.getElementById("exampleModalToggle");
+  const listcontainer = document.getElementById("list-container");
+  if(inputTitle){
+    // console.log(inputTitle);
+    //window.location.href = 'list.html';
+    modalcon.style.display = 'none';
+    createList.classList.add('d-none');
+    const backdropdiv = document.getElementsByClassName("modal-backdrop")[0];
+    backdropdiv.id = "modal-bac";
+    document.getElementById("modal-bac").classList.add("d-none");
+    listcontainer.innerHTML = `<h1>${inputTitle}</h1>`;
+  }
+  else{
+    document.getElementById("error-msg").innerText= "Please enter title of your list";
+    //listcontainer.classList.remove= 'd-none';
+  }
 });
